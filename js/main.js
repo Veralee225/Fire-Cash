76d5e4f8632792cd19d26455b676f78879e3102a
Veralee225 <59749997+Veralee225@users.noreply.github.com>
2020-03-23 02:17:47 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Initialize and add the app
+function initMap() {
+    //Your location
+    const loc = { lat:6.570652, lng:3.367890 };
+    //Centered map on location
+    const map = new google.maps.Map(document.querySelector('.Map')
+    , {
+        zoom: 14,
+        center: loc
+});
+//The marker, positioned at location
+const marker = new google.maps.Marker({ position: loc, map:
+    map});
+}
